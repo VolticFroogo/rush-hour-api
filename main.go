6213994b72afb65a192f41c7d45e0500f7fb14d5
Harry Billings <harry@froogo.co.uk>
2021-02-01 14:48:07 +0000

    Added game 40 for testing
    Added game 40 to test performance. My computer running an i5-7600k @ 4.8GHz solves this in 1.5ms.

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 )
 
 const (
-	game1Cars         uint64 = 0x3222777777774001
-	game1Orientations uint64 = 0xD0B8000000001853
+	game1Cars          uint64 = 0x3222777777774001
+	game1Orientations  uint64 = 0xD0B8000000001853
+	game40Cars         uint64 = 0x7203773004133413
+	game40Orientations uint64 = 0x0D28000A01B9AC53
 )
 
 func main() {
-	game := solver.NewGame(game1Cars, game1Orientations)
+	game := solver.NewGame(game40Cars, game40Orientations)
 	fmt.Println("Initial bitmap:")
 	game.Positions[0].DebugPrintBitmap()
 
